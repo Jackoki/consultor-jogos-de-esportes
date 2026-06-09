16b93a4423dd4235688bbab83e2790cfddecd10c
Jackoki <kenji.inoue@hotmail.com>
2026-06-09 19:20:09 -0300

    Realização de DTO e DateFilter para realização de filtro de eventos esportivos, considerando 3 possibilidade, dia atual, dia especifico, semana especifica.

---
+++ DTOs/DTOFilterDates.cs
@@ -0,0 +1,11 @@
+﻿using consultor_jogos_de_esportes.Utils;
+
+namespace consultor_jogos_de_esportes.DTOs
+{
+    public class DTOFilterDates
+    {
+        public DateFilterType dateFilterType { get; set; }
+
+        public DateTime? Date {  get; set; }
+    }
+}
---
+++ Utils/DateFilterType.cs
@@ -0,0 +1,9 @@
+﻿namespace consultor_jogos_de_esportes.Utils
+{
+    public enum DateFilterType
+    {
+        Today = 1,
+        SpecificDate = 2,
+        Week = 3
+    }
+}
