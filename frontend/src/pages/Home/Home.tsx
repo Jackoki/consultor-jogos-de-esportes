4f1940b8ff9932e958fb8875c8eea0406f94a7f9
Jackoki <kenji.inoue@hotmail.com>
2026-06-17 22:43:20 -0300

    Criação da página Home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,1 +1,21 @@
 import { useState } from "react";
+import { FilterForm } from "../../components/FilterForm/FilterForm";
+import { EventCard } from "../../components/EventCard/EventCard";
+import type { SportEvent } from "../../types/SportEvent";
+
+export function Home() {
+    const [events, setEvents] = useState<SportEvent[]>([]);
+
+    return (
+        <>
+            <h1>Consultor de Eventos Esportivos</h1>
+            <FilterForm setEvents={setEvents} />
+            
+            <div>
+                {events.map(event => (
+                    <EventCard key={`${event.eventName}-${event.beginDate}`} event={event}/>
+                ))}
+            </div>
+        </>
+    );
+}
