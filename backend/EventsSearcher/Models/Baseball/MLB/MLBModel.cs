67f54d14c4a28d817699c93667cfcc83c94d1644
Jackoki <kenji.inoue@hotmail.com>
2026-07-12 10:43:32 -0300

    Correção do resgate de API do MLB

--- backend/EventsSearcher/Models/Baseball/MLB/MLBModel.cs
+++ backend/EventsSearcher/Models/Baseball/MLB/MLBModel.cs
@@ -1,37 +1,60 @@
-﻿namespace consultor_jogos_de_esportes.Models.Baseball
+﻿using System.Text.Json.Serialization;
+
+namespace consultor_jogos_de_esportes.Models.Baseball
 {
     public class MLBModel
     {
+        [JsonPropertyName("gamePk")]
         public int GamePk { get; set; }
+
+        [JsonPropertyName("gameDate")]
         public DateTime GameDate { get; set; }
+
+        [JsonPropertyName("teams")]
         public MLBTeams Teams { get; set; } = new();
+
+        [JsonPropertyName("venue")]
         public MLBVenue Venue { get; set; } = new();
+
         public DateTime DateTimeStart => GameDate;
+
         public DateTime DateTimeEnd => GameDate.AddHours(4);
+
         public string NameEvent =>$"{Teams.Away.Team.Name} x {Teams.Home.Team.Name}";
+
         public string CountryName => "USA";
     }
 
     public class MLBTeams
     {
+        [JsonPropertyName("away")]
         public MLBTeamSide Away { get; set; } = new();
+
+        [JsonPropertyName("home")]
         public MLBTeamSide Home { get; set; } = new();
     }
 
     public class MLBTeamSide
     {
+        [JsonPropertyName("team")]
         public MLBTeam Team { get; set; } = new();
     }
 
     public class MLBTeam
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string Name { get; set; } = string.Empty;
     }
 
     public class MLBVenue
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string Name { get; set; } = string.Empty;
     }
 }
--- backend/EventsSearcher/Models/Baseball/MLB/MLBResponse.cs
+++ backend/EventsSearcher/Models/Baseball/MLB/MLBResponse.cs
@@ -1,14 +1,19 @@
-﻿namespace consultor_jogos_de_esportes.Models.Baseball
+﻿using System.Text.Json.Serialization;
+
+namespace consultor_jogos_de_esportes.Models.Baseball
 {
     public class MLBResponse
     {
+        [JsonPropertyName("dates")]
         public List<MLBDate> Dates { get; set; } = new();
     }
 
     public class MLBDate
     {
+        [JsonPropertyName("date")]
         public string Date { get; set; } = string.Empty;
 
+        [JsonPropertyName("games")]
         public List<MLBModel> Games { get; set; } = new();
     }
 }
