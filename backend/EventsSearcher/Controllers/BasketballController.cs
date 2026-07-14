72c01663aa5395347e0e6e4f68d9e6eabc16e901
Jackoki <kenji.inoue@hotmail.com>
2026-07-14 14:16:48 -0300

    Criação da estrutura base do Controller e Model de jogos da NBA e Basketball

---
+++ backend/EventsSearcher/Controllers/BasketballController.cs
@@ -0,0 +1,39 @@
+﻿
+
+using consultor_jogos_de_esportes.DTOs;
+using consultor_jogos_de_esportes.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace consultor_jogos_de_esportes.Controllers
+{
+    [ApiController]
+    [Route("api/basketball")]
+    public class BasketballController : ControllerBase
+    {
+        private readonly BaseballService _basketballService;
+
+        public BasketballController(BaseballService basketballService)
+        {
+            this._basketballService = basketballService;
+        }
+
+        [HttpPost("events")]
+        public async Task<IActionResult> GetEvents([FromBody] DTOFilterDates filter)
+        {
+            try
+            {
+                var result = await _basketballService.GetEventsAsync(filter);
+                return Ok(result);
+            }
+
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new
+                {
+                    message = ex.Message
+                });
+            }
+        }
+
+    }
+}
---
+++ backend/EventsSearcher/Models/Basketball/NBAModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json.Serialization;
+
+namespace consultor_jogos_de_esportes.Models.Baseball
+{
+    public class NBAModel
+    {
+        [JsonPropertyName("events")]
+        public List<NBAEvent> Events { get; set; } = new();
+    }
+
+    public class NBAEvent
+    {
+        [JsonPropertyName("competitions")]
+        public List<NBACompetition> Competitions { get; set; } = new();
+    }
+
+    public class NBACompetition
+    {
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("venue")]
+        public NBAVenue Venue { get; set; } = new();
+
+        [JsonPropertyName("competitors")]
+        public List<NBACompetitor> Competitors { get; set; } = new();
+    }
+
+    public class NBAVenue
+    {
+        [JsonPropertyName("fullName")]
+        public string Stadium { get; set; }
+
+        [JsonPropertyName("address")]
+        public NBAAddress Address { get; set; } = new();
+    }
+
+    public class NBAAddress
+    {
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+    }
+
+    public class NBACompetitor
+    {
+        [JsonPropertyName("team")]
+        public NBATeam Team { get; set; } = new();
+    }
+    public class NBATeam
+    {
+        [JsonPropertyName("displayName")]
+        public string displayName { get; set; }
+    }
+}
