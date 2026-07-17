8c08322204cdb39f502c57a73a00d9cb83123290
Jackoki <kenji.inoue@hotmail.com>
2026-07-17 14:10:46 -0300

    Criação do Controller e sua rota para requisição do Health-Checker

---
+++ backend/EventsSearcher/Controllers/HealthController.cs
@@ -0,0 +1,25 @@
+﻿using consultor_jogos_de_esportes.HealthChecks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace consultor_jogos_de_esportes.Controllers
+{
+    [ApiController]
+    [Route("api/health-check")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ApiHealthManager apiHealthManager;
+
+        public HealthController(ApiHealthManager apiHealthManager)
+        {
+            this.apiHealthManager = apiHealthManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await apiHealthManager.ValidateAllAsync();
+
+            return Ok(result);
+        }
+    }
+}
