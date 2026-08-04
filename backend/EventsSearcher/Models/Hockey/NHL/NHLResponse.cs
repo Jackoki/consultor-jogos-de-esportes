46d8f7e61244f711878af6eec95424c8c857b21f
Jackoki <kenji.inoue@hotmail.com>
2026-08-03 21:03:59 -0300

    Estrutura base do NHL Service e do NHL Response

---
+++ backend/EventsSearcher/Models/Hockey/NHL/NHLResponse.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json.Serialization;
+
+namespace consultor_jogos_de_esportes.Models.Hockey;
+
+public class NHLResponse
+{
+    [JsonPropertyName("gameWeek")]
+    public List<NHLGameWeek> GameWeek { get; set; } = [];
+}
+
+public class NHLGameWeek
+{
+    [JsonPropertyName("date")]
+    public string Date { get; set; } = string.Empty;
+
+    [JsonPropertyName("games")]
+    public List<NHLGame> Games { get; set; } = [];
+}
+
+public class NHLGame
+{
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+
+    [JsonPropertyName("startTimeUTC")]
+    public DateTime StartTimeUTC { get; set; }
+
+    [JsonPropertyName("venue")]
+    public NHLVenue Venue { get; set; } = new();
+
+    [JsonPropertyName("awayTeam")]
+    public NHLTeam AwayTeam { get; set; } = new();
+
+    [JsonPropertyName("homeTeam")]
+    public NHLTeam HomeTeam { get; set; } = new();
+
+    public DateTime DateTimeStart => StartTimeUTC;
+
+    public DateTime DateTimeEnd => StartTimeUTC.AddHours(3);
+
+    public string NameEvent => $"{AwayTeam.PlaceName.Default} {AwayTeam.CommonName.Default} x " + $"{HomeTeam.PlaceName.Default} {HomeTeam.CommonName.Default}";
+
+    public string CountryName => "USA";
+}
+
+public class NHLVenue
+{
+    [JsonPropertyName("default")]
+    public string Name { get; set; } = string.Empty;
+}
+
+public class NHLTeam
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("abbrev")]
+    public string Abbrev { get; set; } = string.Empty;
+
+    [JsonPropertyName("commonName")]
+    public NHLLocalizedName CommonName { get; set; } = new();
+
+    [JsonPropertyName("placeName")]
+    public NHLLocalizedName PlaceName { get; set; } = new();
+}
+
+public class NHLLocalizedName
+{
+    [JsonPropertyName("default")]
+    public string Default { get; set; } = string.Empty;
+}
---
+++ backend/EventsSearcher/Services/Hockey/NHLService.cs
@@ -0,0 +1,79 @@
+﻿using consultor_jogos_de_esportes.DTOs;
+using consultor_jogos_de_esportes.Models;
+using consultor_jogos_de_esportes.Models.AmericanFootball;
+using consultor_jogos_de_esportes.Utils;
+
+namespace consultor_jogos_de_esportes.Services.Basketball
+{
+    public class NHLService : ApiSportService
+    {
+        public override string SportName => "nHl";
+        public NHLService(HttpClient httpClient) : base(httpClient)
+        {
+        }
+
+        public override async Task<List<SportEventModel>> GetEventsAsync(DTOFilterDates filter)
+        {
+            var dates = GetDatesFilter(filter);
+            var urls = BuildUrls(dates, filter.DateFilterType);
+            var tasks = urls.Select(url => GetAsync<NFLResponse>(url));
+            var responses = await Task.WhenAll(tasks);
+
+            var competitions = responses
+                .Where(r => r != null)
+                .SelectMany(r => r!.Events)
+                .SelectMany(e => e.Competitions)
+                .ToList();
+
+            return MapToSportEvents(competitions);
+        }
+
+        private List<string> BuildUrls(DateRange dates, DateFilterType filterType)
+        {
+            if (filterType == DateFilterType.Today || filterType == DateFilterType.SpecificDate)
+            {
+                return new() {
+                    $"https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?dates={dates.StartDate:yyyyMMdd}"
+                };
+            }
+
+            var urls = new List<string>();
+
+            for (var data = dates.StartDate.Date; data <= dates.EndDate.Date; data = data.AddDays(1))
+            {
+                urls.Add($"https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?dates={data:yyyyMMdd}");
+            }
+
+            return urls;
+        }
+
+        private List<SportEventModel> MapToSportEvents(List<NFLCompetition> competitions)
+        {
+            var events = new List<SportEventModel>();
+
+            foreach (var competition in competitions)
+            {
+                var home = competition.Competitors.FirstOrDefault(c => c.HomeAway == "home");
+
+                var away = competition.Competitors.FirstOrDefault(c => c.HomeAway == "away");
+
+                if (home == null || away == null)
+                    continue;
+
+                events.Add(new SportEventModel
+                {
+                    SportName = "Futebol Americano NFL",
+                    EventName = $"{home.Team.DisplayName} vs {away.Team.DisplayName}",
+                    BeginDate = DateTimeUtils.ToBrazilTime(competition.Date),
+                    EndDate = DateTimeUtils.ToBrazilTime(competition.Date).AddHours(3),
+                    Location = competition.Venue.Address.City,
+                    HasTime = true,
+                    LeftImage = home.Team.Logo,
+                    RightImage = away.Team.Logo
+                });
+            }
+
+            return events;
+        }
+    }
+}
